1a5c6708c7d8da453f9efc8f0629275319f67ba5
tom <tom123@wz.com>
2018-12-23 18:53:13 +0800

    edit

--- src/main/java/www/wangzhi/demo/GitDemo.java
+++ src/main/java/www/wangzhi/demo/GitDemo.java
@@ -2,7 +2,7 @@ package www.wangzhi.demo;
 
 public class GitDemo {
 	public static void main(String[] args) {
-		System.out.println("rigth...");
+		System.out.println("left....");
 		System.out.println("rigth...");
 	}
 }
